ddb0143a22840661b29e06602fc3bb8e56700e72
Anita Tiwari <anit8621@stud.kea.dk>
2022-06-22 10:43:29 +0200

    vite config

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                book: resolve(__dirname, 'booking1.html'),
+                ticket: resolve(__dirname,'ticket.html')
+            }
+        }
+    }
+})
